10f7388b9b6f9a8cebb428bd52f14107a9f5a485
falcon-hayaki <allrightprincekin@outlook.com>
2023-01-27 12:08:33 +0800

    fixed

--- stop.sh
+++ stop.sh
@@ -1,4 +1,6 @@
-killall python --wait
+PID=$(ps -ef | grep "python3 run.py" | grep -v grep | awk '{ print $2 }')
+echo kill -9 $PID
+kill -9 $PID
 
 PID=$(ps -ef | grep "bash record" | grep -v grep | awk '{ print $2 }')
 if [ -z "$PID" ]
